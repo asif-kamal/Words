92cfcabb211c9955a99745fac4bcabe6b6c18597
asif-kamal <asif.kamal1489@gmail.com>
2021-08-14 14:47:00 -0400

    Got arguments in WordListFragment.kt

--- app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -22,6 +22,15 @@ class WordListFragment : Fragment() {
     }
     private var _binding: FragmentWordListBinding? = null
     private val binding get() = _binding!!
+    private lateinit var letterId: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            letterId = it.getString(LETTER).toString()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,7 +45,7 @@ class WordListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val recyclerView = binding.recyclerView
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.adapter = WordAdapter(activity?.intent?.extras?.getString(LETTER).toString(), requireContext())
+        recyclerView.adapter = WordAdapter(letterId, requireContext())
 
         recyclerView.addItemDecoration(
             DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
